3072cb32bc0938e85ff17631d2bf8ea229d3f405
B-Enterprise <152478482+B-Enterprise@users.noreply.github.com>
2024-03-06 04:17:59 -0600

    FORMAT CODE

--- Login/src/com/Proyecto/Login.java
+++ Login/src/com/Proyecto/Login.java
@@ -394,7 +394,7 @@ public class Login extends javax.swing.JFrame {
     }//GEN-LAST:event_jPanel5MouseClicked
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-    Register newframe = new Register();
+        Register newframe = new Register();
         newframe.setVisible(true);
         this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
